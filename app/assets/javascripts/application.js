3c69fff8a787fbaa70242643176bda46433cb60e
Jeff Wolfram <jeffwolfram@gmail.com>
2017-12-18 21:42:12 -0800

    first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,65 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require turbolinks
+//= require_tree .
+var introMessage = "Countdown to 2018";
+var message = "Happy New Year";
+// set the date we're counting down to
+var target_date = new Date("Dec 17, 2017" ).getTime();
+ var year = new Date().getFullYear() + 1;
+// variables for time units
+var days, hours, minutes, seconds;
+
+ var intro = document.getElementById("intro");
+// get tag element
+var countdown = document.getElementById("countdown");
+//var test = 0;
+// update the tag with id "countdown" every 1 second
+var promise = setInterval(function () {
+//    test++;
+
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+
+    // do some time calculations
+    days = parseInt(seconds_left / 86400);
+    seconds_left = seconds_left % 86400;
+
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+
+    // format countdown string + set tag value
+    if(days === 0 && hours === 0 && minutes === 0 && seconds === 0){
+        intro.innerHTML = message;
+        countdown.innerHTML = '';
+        clearInterval(promise);
+    } else if(days === 0 && hours === 0 && minutes === 0){
+        intro.innerHTML = introMessage;
+        countdown.innerHTML =   seconds + "s ";
+    } else if(days === 0 && hours === 0){
+        intro.innerHTML = introMessage;
+        countdown.innerHTML = minutes + "m " + seconds + "s ";
+    } else if( days === 0){
+        intro.innerHTML = introMessage;
+        countdown.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+    }else {
+    intro.innerHTML = introMessage;
+        countdown.innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";   }
+
+}, 1000);
---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+
+  # For detais on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcomes#index'
+
+end
